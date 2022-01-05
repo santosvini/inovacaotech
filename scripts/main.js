0f6449b63a90ff1b0155777416cf25f1a9fc7a24
Vini Santos <vini_santos7@live.com>
2022-01-05 20:33:29 -0300

    :construct::hammer::recycle: refactor code, about position stylesheet, and best practice from HTML :construct::hammer::recycle:

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
-var contato = document.getElementById('contato')
-  contato.addEventListener('mousenter', entrar)
-  contato.style.background = 'white'
+/* var contato = document.getElementById('contato')
+  
+  contato.addEventListener('click', entrar)
+  contato.style = ''
     function entrar() {
-      contato.innerText = 'Faça seu orçamento com a Inovação Tech'
-      contato.innerHTML = ''
-        }
+    contato.innerText = 'Faça seu orçamento com a Inovação Tech'
+    }
+
+var btn = document.querySelector("#topo");
+
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+}); */
