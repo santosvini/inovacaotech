185abad09a04ec2fb757d71cf3c03595fd2d06e9
Vini Santos <vini_santos7@live.com>
2022-01-08 22:12:05 -0300

    :art::fire::sparkles: update: separete sections accordingly with semantic, adjustment the stylesheet, refactor code in JS, and a create a toggle. :art::fire::sparkles:

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@
   contato.addEventListener('click', entrar)
   contato.style = ''
     function entrar() {
-    contato.innerText = 'Entre em contato com a Inovação Tech'
+    contato.innerText = 'Faça seu orçamento com a Inovação Tech'
     }
 
 var btn = document.querySelector("#topo");
@@ -11,3 +11,18 @@ var btn = document.querySelector("#topo");
 btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
 }); */
+
+const change = document.querySelector('.btn')
+
+change.addEventListener('click', function() {
+  document.body.classList.toggle('dark-theme')
+
+  var className = document.body.className;
+  if (className == 'light-theme') {
+    this.textContent = 'Dark';
+  } 
+  else {
+    this.textContent = 'Light';
+  }
+
+})
