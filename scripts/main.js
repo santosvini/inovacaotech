c5ca0900f87893a3de583b00abe8667da6c245ec
Vini Santos <vini_santos7@live.com>
2022-01-09 21:35:32 -0300

    :sparkles::recycle: add interactivy on the page, refactor stylesheet and index. :sparkles::recycle:

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,34 @@
-/* var contato = document.getElementById('contato')
+var contato = document.getElementById('contato')
+var p = document.querySelector("section#contato")
   
-  contato.addEventListener('click', entrar)
-  contato.style = ''
-    function entrar() {
-    contato.innerText = 'Faça seu orçamento com a Inovação Tech'
-    }
+  contato.addEventListener('click', function entrar() {
+  contato.style.background = 'var(--cor0)'
+  contato.innerText = 'Faça seu orçamento com a Inovação Tech'
+})
 
-var btn = document.querySelector("#topo");
-
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-}); */
+  contato.addEventListener('mouseout', function sair() {
+  contato.style = 'var(--cor0)'
+  contato.innerHTML = `
+  <section id="contato">
+  <h2>Contato</h2>
+  <p>Fale Conosco</p>
+  <div class="container-media">
+    <div class="social-media">
+      <a
+        href="https://api.whatsapp.com/message/JTUW4GXA2MVFB1"
+        target="_blank">
+        <img src="./imagens/whatsapp.png" alt="telefone" />
+        <p>(11) 92000-0319</p>
+      </a>
+      <a href="mailto:inovacaotech@outlook.com.br">
+        <img src="./imagens/mail.png" alt="e-mail"/><p>inovacaotech@outlook</p></a>
+      <a href="https://www.instagram.com/inovacaotech010/" target="_blank">
+        <img src="./imagens/instagram.png" alt="instagram"/>
+        <p>@inovacaotech010</p></a>
+    </div>
+  </div>
+</section>`
+})
 
 const change = document.querySelector('.btn')
 
